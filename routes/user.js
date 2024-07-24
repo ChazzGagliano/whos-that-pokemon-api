55e9ed6801bb16ad58b4dc97632233f3aca76eae
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-24 16:48:09 -0400

    add signup route to user backend

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,25 @@
 import { Router } from "express";
+import { users } from "../config/mongoCollections.js";
+import bcrypt from "bcrypt";
 
-const router = Router()
 
+const router = Router();
+router.post("/signup", async (req, res) => {
+  const userCollection = await users();
+  let hashedPassword = await bcrypt.hash(req.body.password, 12);
+  let user = {
+    username: req.body.username,
+    email: req.body.email,
+    password: hashedPassword,
+  };
+  const checkIfUserExists = await userCollection.findOne({
+    email: req.body.email,
+  });
+  if (checkIfUserExists !== null)
+    return res.json({ error: "An account with that email already exists" });
 
+  await userCollection.insertOne(user);
+  return res.status(200).json(user);
+});
 
-export default router
+export default router;
