e6d365b5df1e804943ea70f644ef67d9ad37f964
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-24 16:37:37 -0400

    set up user route in index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import hardRoutes from "./hard.js"
 import johtoRoutes from "./johto.js"
 import pokedexRoutes from "./pokedex.js"
 import pokemonRoutes from "./pokemon.js"
+import userRoutes from "./user.js"
 
 const constructorMethod = (app) => {
     app.use("/easy", easyRoutes);
@@ -10,6 +11,7 @@ const constructorMethod = (app) => {
     app.use("/johto", johtoRoutes)
     app.use("/pokedex", pokedexRoutes)
     app.use("/pokemon", pokemonRoutes)
+    app.use("/user")
     app.use("*", (req, res) => {
       res.status(404).json({ error: "This route does not exist" });
     });
