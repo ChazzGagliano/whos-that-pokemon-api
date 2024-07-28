b2dcd1f5cb0a022edf3d7c5c444080eb40332e76
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-07-28 15:21:16 -0400

    add logout route to User backend

--- routes/user.js
+++ routes/user.js
@@ -23,32 +23,47 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    const userCollection = await users();
-    const { userName } = req.body;
-    const { passWord } = req.body;
-    const user = await userCollection.findOne({
-      username: userName,
-    });
-  
-    console.log(passWord);
-    console.log(user);
-    let compareToMatch = false;
-  
-    try {
-      compareToMatch = await bcrypt.compare(passWord, user.password);
-    } catch (e) {}
-    if (!compareToMatch)
-      return res.json({ error: "Either the email or password is invalid" });
-    console.log({ success: "User authenticated" });
-  
-    user._id = user._id.toString();
-  
-    req.session.user = user;
-    req.session.save();
-    console.log(req.session);
-  
-    res.json({ user: user, auth: true });
+  const userCollection = await users();
+  const { userName } = req.body;
+  const { passWord } = req.body;
+  const user = await userCollection.findOne({
+    username: userName,
   });
 
+  console.log(passWord);
+  console.log(user);
+  let compareToMatch = false;
+
+  try {
+    compareToMatch = await bcrypt.compare(passWord, user.password);
+  } catch (e) {}
+  if (!compareToMatch)
+    return res.json({ error: "Either the email or password is invalid" });
+  console.log({ success: "User authenticated" });
+
+  user._id = user._id.toString();
+
+  req.session.user = user;
+  req.session.save();
+  console.log(req.session);
+
+  res.json({ user: user, auth: true });
+});
+
+router.get("/logout", async (req, res) => {
+  console.log(req.session);
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ error: "Failed to log out. Please try again." });
+      }
+      return res.status(200).json({ message: "successfully logged out" });
+    });
+  } else {
+    return res.status(401).json({ error: "You are not logged in!" });
+  }
+});
 
 export default router;
