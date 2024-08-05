715a56e6a999e353f6d80af27b6f74b7b37e6471
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-05 15:20:42 -0400

    remove email from frontend and complete full stack signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
 const Signup = () => {
-  const [userName, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [userName, setuserName] = useState("");
+  const [passWord, setpassWord] = useState("")
+  const navigate = useNavigate()
+
 
   const handleSignup = async (event) => {
-    event.prevent.default();
-    await axios.post("http://localhost:3030/user/signup",
-      {
-        username: userName,
-        email: email,
-    });
+    event.preventDefault();
+    await axios.post("http://localhost:3030/user/signup", {
+       username: userName,
+       password: passWord
+    })
+    navigate(`/`)
   };
-};
 
-export default Signup;
+  return (
+    <div id="signup">
+      <h1 className="welcome">Signup</h1>
+      <form onSubmit={handleSignup}>
+        <div className="welcome">
+        <div>
+          Username: <input name="username" type="text" value={userName} onChange={(event) => {setuserName(event.target.value)}} />
+        </div>
+        <div>
+          Password: <input name="password" type="password" value={passWord} onChange={(event) => {setpassWord(event.target.value)}} />
+        </div>
+        </div>
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+
+}
+
+export default Signup
