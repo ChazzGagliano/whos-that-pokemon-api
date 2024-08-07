a085febca4d4b8f2c8de4e82910d72ec98904f39
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-07 15:14:42 -0400

    install express.session package

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,27 +1,34 @@
-import React, { useState} from "react"
+import React, { useState } from "react";
 import axios from "axios"
-import Link from "react-router-dom"
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
-const [userName, setuserName] = useState("")
-const [passWord, setpassWord] = useState("")
+  const [userName, setuserName] = useState("");
+  const [passWord, setpassWord] = useState("")
 
-const handleLogin = async (e) => {
+
+
+  const handleLogin = async (e) => {
     e.preventDefault();
     const response = await axios.post("http://localhost:3030/user/login", {
-        userName: userName,
-        passWord, passWord
+       userName: userName,
+       passWord: passWord
     }, {withCredentials: true})
 
     console.log(response)
-}
+    
+    if (response.data.auth) {
+        window.location.href = "/"
+    }
+    
+  };
 
-    return (
-        <div id="">
-            <h1 className="welcome">Login</h1>
-            <form onSubmit={handleLogin}>
-            <div className="welcome">
+  return (
+    <div id="signup">
+      <h1 className="welcome">Login</h1>
+      <form onSubmit={handleLogin}>
+        <div className="welcome">
         <div>
           Username: <input name="username" type="text" value={userName} onChange={(event) => {setuserName(event.target.value)}} />
         </div>
@@ -30,9 +37,10 @@ const handleLogin = async (e) => {
         </div>
         </div>
         <button type="submit">Login</button>
-            </form>
-        </div>
-    )
+      </form>
+    </div>
+  );
+
 }
 
 export default Login
