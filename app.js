5037b4e8b0b6d602521552c932299f9822c633b1
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-07 15:14:58 -0400

    update app.js for cors

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ import express from "express";
 const app = express();
 import configRoutes from "./routes/index.js";
 import cors from "cors";
+import session from 'express-session';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+        credentials: true, origin: 'http://localhost:3000'
+    }));
+app.use(session({
+    name: 'AuthCookie', secret: 'apple bottom jeans', resave: false, saveUninitialized: false, cookie: {
+        httpOnly: false, secure: false
+    }
+}));
 
 configRoutes(app);
 
