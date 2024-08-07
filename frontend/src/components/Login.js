38033e9a8786363212f368b4ef7b47170d4f3799
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-07 14:59:34 -0400

    create handlelogin in Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,21 @@
-
+import React, { useState} from "react"
+import axios from "axios"
+import Link from "react-router-dom"
 
 
 const Login = () => {
+const [userName, setuserName] = useState("")
+const [passWord, setpassWord] = useState("")
 
+const handleLogin = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:3030/user/login", {
+        userName: userName,
+        passWord, passWord
+    }, {withCredentials: true})
+
+    console.log(response)
+}
 }
 
 export default Login
