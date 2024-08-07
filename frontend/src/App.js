67d3f450c8dbff122f41f36208e294029724f59f
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-07 14:55:06 -0400

    Import Login.js to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Johto from "./components/Johto"
 import Pokedex from "./components/Pokedex.js"
 import Pokemon from "./components/Pokemon.js"
 import Signup from "./components/Signup.js"
+import Login from "./components/Login.js"
 import Navigation from "./components/Navigation.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -24,6 +25,7 @@ function App() {
               <Route path="/pokedex" element={<Pokedex />} />
               <Route path="/pokemon/:id" element={<Pokemon />} />
               <Route path="/signup" element={<Signup/>} />
+              <Route Path="/login" element={<Login/>}/>
             </Routes>
           </div>
         </div>
