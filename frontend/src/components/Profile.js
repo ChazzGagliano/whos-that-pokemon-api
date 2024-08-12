efc42704a84aef965e52e66c642fc92ed68e2dc6
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-12 16:42:24 -0400

    format code

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -6,49 +6,45 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 const Profile = () => {
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchData() {
-            const data = await axios.get(`http://localhost:3030/user/profile`, {
-                withCredentials: true,
-            })
-            setUser(data);
-            console.log(data)
-            setLoading(false)
-        }
-        if (user === null) {
-            fetchData()
-        }
-    }, [user])
-
-    if (loading) {
-        return <Loading /> 
-    } else {
-        if (user.data.error) {
-            return (
-                <div>
-            <div>
-              <Link className="" to={`/signup`}>
-                <h1>New? Sign up!</h1>
-              </Link>
-            </div>
-            <div>
-              <Link className="" to={`/login`}>
-                <h1>Already a Member? Login!</h1>
-              </Link>
-            </div>
-          </div>
-        )
-    } else {
-        return (
-            <div>
-                {user.data.user.username}
-            </div>
-        )
+  useEffect(() => {
+    async function fetchData() {
+      const data = await axios.get(`http://localhost:3030/user/profile`, {
+        withCredentials: true,
+      });
+      setUser(data);
+      console.log(data);
+      setLoading(false);
     }
-    } 
-}
+    if (user === null) {
+      fetchData();
+    }
+  }, [user]);
+
+  if (loading) {
+    return <Loading />;
+  } else {
+    if (user.data.error) {
+      return (
+        <div>
+          <div>
+            <Link className="" to={`/signup`}>
+              <h1>New? Sign up!</h1>
+            </Link>
+          </div>
+          <div>
+            <Link className="" to={`/login`}>
+              <h1>Already a Member? Login!</h1>
+            </Link>
+          </div>
+        </div>
+      );
+    } else {
+      return <div>{user.data.user.username}</div>;
+    }
+  }
+};
 
 export default Profile;
