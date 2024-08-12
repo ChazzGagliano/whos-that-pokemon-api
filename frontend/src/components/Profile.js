bd53a252ab43c076eb0fe088d9abd569699b4914
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-12 16:32:44 -0400

    set up profile request in profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,7 +1,23 @@
-
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Loading from "./Loading";
+import axios from "axios";
 
 const Profile = () => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
 
+    useEffect(() => {
+        async function fetchData() {
+            const data = await axios.get(`http://localhost:3030/user/profile`, {
+                withCredentials: true,
+            })
+            setUser(data);
+            console.log(data)
+            setLoading(false)
+        }
+    })
 }
 
 export default Profile;
