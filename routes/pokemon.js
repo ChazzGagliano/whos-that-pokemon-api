ae53e050d96bc0b35e9ce482f9daef29c383eb08
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-26 15:12:18 -0400

    clean code

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,23 +1,20 @@
 import { Router } from "express";
-import axios from "axios"; 
+import axios from "axios";
 
 const router = Router();
 
 router.get(`/:id`, async (req, res) => {
-    const { id } = req.params
-    const { data } = await axios.get(
-        `https://pokeapi.co/api/v2/pokemon/${id}`
-      );
-      res.json(data);
+  const { id } = req.params;
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  res.json(data);
 });
 
 router.get(`/:search`, async (req, res) => {
-    const { searchTerm } = req.params
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`
-)
-res.json(data)
-})
-
-
+  const { searchTerm } = req.params;
+  const { data } = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon/${searchTerm}`
+  );
+  res.json(data);
+});
 
 export default router;
