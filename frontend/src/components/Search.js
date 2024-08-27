72750ce789fd61bbe68f70acdf7e2d955604b2d0
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-27 17:20:58 -0400

    set up axios request

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -2,12 +2,33 @@ import axios from "axios";
 import React, { useState } from "react";
 
 const Search = () => {
+    const [pokemon, setPokemon] = useState("")
+    const [term, setTerm] = useState([])
+
+
+
+    const submitPokemon = async (e) => {
+        e.preventDefault();
+    
+        if (pokemon === "") {
+          return;
+        }
+    
     
 
+    const { data } = await axios.get(
+        `http://localhost:3030/pokemon/${pokemon}`
+      );
+      setTerm(data);
+      console.log(data);
 
-
-
- 
-};
-
+    };
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+    
 export default Search;
