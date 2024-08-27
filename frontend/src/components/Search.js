5190410b962542a11497b36eda8425b72878caff
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-27 17:39:54 -0400

    clean code

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,54 +3,46 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Search = () => {
-    const [pokemon, setPokemon] = useState("")
-    const [term, setTerm] = useState([])
+  const [pokemon, setPokemon] = useState("");
+  const [term, setTerm] = useState([]);
 
+  const submitPokemon = async (e) => {
+    e.preventDefault();
 
-
-    const submitPokemon = async (e) => {
-        e.preventDefault();
-    
-        if (pokemon === "") {
-          return;
-        }
-    
-    
+    if (pokemon === "") {
+      return;
+    }
 
     const { data } = await axios.get(
-        `http://localhost:3030/pokemon/${pokemon}`
-      );
-      setTerm(data);
-      console.log(data);
+      `http://localhost:3030/pokemon/${pokemon}`
+    );
+    setTerm(data);
+    console.log(data);
+  };
 
-    };
-    
-    return (
+  return (
+    <div>
+      <h1>Explore!</h1>
+      <input
+        className="search-bar"
+        type="text"
+        onChange={(e) => setPokemon(e.target.value)}
+        value={pokemon}
+      />
+      <button className="" onClick={submitPokemon}>
+        search
+      </button>
+      {term && (
         <div>
-            <h1>Explore!</h1>
-        <input
-          className="search-bar"
-          type="text"
-          onChange={(e) => setPokemon(e.target.value)}
-          value={pokemon}
-        />
-        <button className="" onClick={submitPokemon}>
-          search
-        </button>
-        {term && (
-        <div>
-            <div>
-              <Link to={`/pokemon/${term.name}`} className="">
-                  <div>
-                      {term.name}
-                  </div>
-              </Link>
-            </div>
-        
-      </div>
-        )}
-      </div>
-    )
-}
-    
+          <div>
+            <Link to={`/pokemon/${term.name}`} className="">
+              <div>{term.name}</div>
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default Search;
