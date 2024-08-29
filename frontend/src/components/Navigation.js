bf9e6212ac3734d2acf5a358d5ee1b915af9dd49
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-08-29 16:53:58 -0400

    add pokemon logo to navbar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -3,6 +3,13 @@ import React from "react";
 const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+         <a className="navbar-brand" href="/">
+        <img
+        className="poke-logo"
+        src="https://loodibee.com/wp-content/uploads/International-Pokemon-logo.png"
+        >
+        </img>
+        </a>
       <a className="navbar-brand" href="/">
         Home
       </a>
