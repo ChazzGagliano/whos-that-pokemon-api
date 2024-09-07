f6c582caff18c88acd6cc58f4d8d502a344cab92
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-09-06 21:46:01 -0400

    clean code

--- frontend/src/components/Pokemon.js
+++ frontend/src/components/Pokemon.js
@@ -6,22 +6,21 @@ import Loading from "./Loading";
 import { useParams } from "react-router-dom";
 
 const Pokemon = () => {
-    const { id } = useParams();
-    const [pokemon, setPokemon] = useState(undefined);
-    const [loading, setLoading] = useState(true);
-    const [currentShiny, setCurrentShiny] = useState("");
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const [currentShiny, setCurrentShiny] = useState("");
 
-    useEffect(() => {
-        async function fetchData() {
-          const { data } = await axios.get(`http://localhost:3030/pokemon/${id}`);
-          setPokemon(data);
-          setCurrentShiny(data.sprites.other.showdown.front_default);
-          console.log(data);
-          setLoading(false);
-        }
-        fetchData();
-      }, [id]);
-
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await axios.get(`http://localhost:3030/pokemon/${id}`);
+      setPokemon(data);
+      setCurrentShiny(data.sprites.other.showdown.front_default);
+      console.log(data);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
 
   const handleMouseEnter = () => {
     setCurrentShiny(pokemon.sprites.other.showdown.front_shiny); // Switch to back sprite
@@ -31,55 +30,43 @@ const Pokemon = () => {
     setCurrentShiny(pokemon.sprites.other.showdown.front_default); // Switch back to front sprite
   };
 
-      if (loading) {
-        return <Loading />;
-      } else {
-        return (
-            <div>
-                  <div className="topleft">
-                    <h1>{pokemon.name}</h1>
-                </div>
+  if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
+        <div className="topleft">
+          <h1>{pokemon.name}</h1>
+        </div>
 
-                <div className="topright">
-                    <h6>{pokemon.stats[0].stat.name}</h6> <h4>{pokemon.stats[0].base_stat}</h4>
-                </div>
-                <div>
-                <img className="easy-gif"
-                src={currentShiny}
+        <div className="topright">
+          <h6>{pokemon.stats[0].stat.name}</h6>{" "}
+          <h4>{pokemon.stats[0].base_stat}</h4>
+        </div>
+        <div>
+          <img
+            className="easy-gif"
+            src={currentShiny}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-                >
-                    </img> 
-                </div>
-                <div>
-                    {pokemon.height} ft {pokemon.weight} lbs
-                </div>
-                <div>
-                   
-                <div>
-                 {pokemon.moves[0].move.name}
-                </div>
-                <div>
-                {pokemon.moves[1].move.name}
-                </div>
-                </div>
-                <div>
-                    Number: {pokemon.order}
-                </div>
-                <div className="bottomleft">
-                {pokemon.types.map((p) => {
-                    return (
-                        <div className="">
-                            {p.type.name}
-                        </div>
-                    )
-                })}
-                   
-                </div>
-
-            </div>
-        )
-    }
-}
+          ></img>
+        </div>
+        <div>
+          {pokemon.height} ft {pokemon.weight} lbs
+        </div>
+        <div>
+          <div>{pokemon.moves[0].move.name}</div>
+          <div>{pokemon.moves[1].move.name}</div>
+        </div>
+        <div>Number: {pokemon.order}</div>
+        <div className="bottomleft">
+          {pokemon.types.map((p) => {
+            return <div className="">{p.type.name}</div>;
+          })}
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Pokemon;
