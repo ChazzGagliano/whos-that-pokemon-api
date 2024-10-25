27482addfb6d16ba399404706b2e6a7731c77ca0
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-10-24 20:09:25 -0400

    change navbar background color

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg  bg-dark">
          <a className="navbar-brand" href="/">
         <img
         className="poke-logo"
