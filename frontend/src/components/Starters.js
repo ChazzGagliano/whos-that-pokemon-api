9a21b1fdc7eaad64978359ceeb52fb76f9eed0bc
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2025-01-08 14:50:24 -0500

    create front end data request

--- frontend/src/components/Starters.js
+++ frontend/src/components/Starters.js
@@ -6,12 +6,27 @@ import Loading from "./Loading";
 
 
 const Starters = () => {
+    const [starter, setStarters] = useState(undefined);
+    const [loading, setLoading] = useState(true);
     
-return (
-    <div>
+useEffect(() => {
+    async function fetchData() {
+        const data = await axios.get(`http://localhost:3030/pokemon/starters`)
+        setStarters(data)
+        setLoading(false)
+    }
+    fetchData();
+}, [])
+
+if (loading) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
         
-    </div>
-)
+      </div>
+    );
+  }
 }
 
 export default Starters;
